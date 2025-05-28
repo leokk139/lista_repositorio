4444813b72203bdf731d87d2934d014f595d2623
leokk139 <leo.rocha340@gmail.com>
2025-05-27 22:47:34 -0400

    Delete ex_8.py

--- ex_8.py
+++
@@ -1,9 +0,0 @@
-'''
-8. Dada a lista cores = ["azul", "verde", "amarelo"],
-insira a cor "vermelho" na posição 1.
-'''
-
-cores = ["azul", "verde", "amarelo"]
-
-cores.insert(1, "vermelho")
-print(cores)
